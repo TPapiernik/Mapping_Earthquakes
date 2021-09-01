3adeb02b0f0989f509a672922642e320031b803f
Thomas Papiernik <tom.papiernik@gmail.com>
2021-09-01 12:22:47 -0500

    Mapping GeoJSON Points Exercise Example Complete

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,56 +3,73 @@
 // Add console.log to check to see if our code is working.
 console.log("Working...");
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
 // Create the map object with a center at the San Francisco airport.
-//let map = L.map('mapid').setView([37.3213, -122.3790], 5);
-let map = L.map('mapid').setView([40.0000, -100.0000], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the polyline
-// Original:
-//let line = [
-//    [33.9416, -118.4085],
-//    [37.6213, -122.3790],
-//    [40.7899, -111.9791],
-//    [47.4502, -122.3088]
-//];
+// ############################################################################
 
-// Skill Drill:
-// Airline Route from San Francisco to John F. Kennedy Int'l Airport
-// via Denver, Austin-Bergstrom, & Toronto Pearson International
-// (Blue Dashed Line, weight of 4 and opacity of 0.5 on the light map)
-let line = [
-    // SFO
-    [33.9416, -118.4085],
-    // DEN
-    [39.8617, -104.6731],
-    // AUS
-    [30.1944, -97.6700],
-    // YYZ
-    [43.6767, -79.6306],
-    // JFK
-    [40.6340, -73.7789]
-];
+// Version 1
+// Grabbing our GeoJSON data.
+//L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    //pointToLayer: function(feature, latlng) {
+      //console.log(feature);
+      //return L.marker(latlng)
+      //.bindPopup("<h2>" + feature.properties.city + "</h2>");
+      // Skill Drill:
+      //.bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+    //}
+//}).addTo(map);
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  //color: "red"
-  //color: "yellow"
-  color: "blue",
-  weight: 4,
-  opacity: 0.5,
-  dashArray: '7,7',
+// ############################################################################
+
+// ############################################################################
+
+// Version 2
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      //layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+      // Skill Drill:
+      layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h2>Airport Name: " + feature.properties.name);
+    }
 }).addTo(map);
+
+// ############################################################################
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    //id: 'mapbox/streets-v11',
+    id: 'mapbox/streets-v11',
     //id: 'mapbox/dark-v10',
     //id: 'mapbox/satellite-streets-v11',
+    //id: 'mapbox/light-v10',
     // Skill Drill:
-    id: 'mapbox/light-v10',
+    //id: 'mapbox/navigation-night-v1',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
