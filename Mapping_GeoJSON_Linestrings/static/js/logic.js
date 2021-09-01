e441dffab1bf124bccd84366e71d03917896e54a
Thomas Papiernik <tom.papiernik@gmail.com>
2021-09-01 13:58:17 -0500

    Mapping GeoJSON Linestrings Example Complete

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -3,69 +3,12 @@
 // Add console.log to check to see if our code is working.
 console.log("Working...");
 
-// Add GeoJSON data.
-//let sanFranAirport =
-//{"type":"FeatureCollection","features":[{
-    //"type":"Feature",
-    //"properties":{
-        //"id":"3469",
-        //"name":"San Francisco International Airport",
-        //"city":"San Francisco",
-        //"country":"United States",
-        //"faa":"SFO",
-        //"icao":"KSFO",
-        //"alt":"13",
-        //"tz-offset":"-8",
-        //"dst":"A",
-        //"tz":"America/Los_Angeles"},
-        //"geometry":{
-            //"type":"Point",
-            //"coordinates":[-122.375,37.61899948120117]}}
-//]};
-
-// ############################################################################
-
-// Version 1
-// Grabbing our GeoJSON data.
-//L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    //pointToLayer: function(feature, latlng) {
-      //console.log(feature);
-      //return L.marker(latlng)
-      //.bindPopup("<h2>" + feature.properties.city + "</h2>");
-      // Skill Drill:
-      //.bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
-    //}
-//}).addTo(map);
-
-// ############################################################################
-
-// ############################################################################
-
-// Version 2
-// Grabbing our GeoJSON data.
-//L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    //onEachFeature: function(feature, layer) {
-      //console.log(layer);
-      //layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
-      // Skill Drill 1:
-      //layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> <h2>Airport Name: " + feature.properties.name + "</h2>");
-    //}
-//}).addTo(map);
-
-// ############################################################################
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    //id: 'mapbox/dark-v10',
-    //id: 'mapbox/satellite-streets-v11',
-    //id: 'mapbox/light-v10',
-    // Skill Drill:
-    //id: 'mapbox/navigation-night-v1',
+    //id: 'mapbox/streets-v11',
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -81,41 +24,40 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
 // Create the map object with  center and zoom level.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Original:
-// Add our 'streets' tile layer to the map.
-//streets.addTo(map);
-
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/TPapiernik/Mapping_Earthquakes/main/majorAirports.json";
+//let airportData = "https://raw.githubusercontent.com/TPapiernik/Mapping_Earthquakes/main/majorAirports.json";
 
-// ############################################################################
+// Accessing the Toronto Routes GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/TPapiernik/Mapping_Earthquakes/main/torontoRoutes.json";
 
-// Version 3
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  //L.geoJson(data).addTo(map);
-  // Skill Drill:
   L.geoJson(data, {
+    style: myStyle,
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr> " + "<h2>Airport Name: " + feature.properties.name + "</h2>")
+      layer.bindPopup("<h3>Airline: " + feature.properties.airline + "</h3> <hr> Destination: " + feature.properties.dst + "</h3>");
     }
   }).addTo(map);
 });
 
-// ############################################################################
